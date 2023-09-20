f536b7c77a46c99843bc21f2010a4c95e56130e0
David Feldner <dafe@itu.dk>
2023-09-20 10:39:21 +0200

    Start using go net package

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+)
 
 func main() {
-	fmt.Print("Hello world!")
+	go listen(25555)
+	go listen(25554)
+	time.Sleep(4 * time.Second)
+	go send(25555)
+	go send(25554)
+	time.Sleep(50 * time.Second)
+
+}
+
+func send(port int) {
+	conn, err := net.Dial("udp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		// handle error
+	}
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+
+	fmt.Print(status)
+}
+
+func listen(port int) {
+	udpAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		fmt.Print(err)
+	}
+	ln, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Print(err)
+	}
+	for {
+		var buf [512]byte
+		_, addr, err := ln.ReadFromUDP(buf[0:])
+		println(addr)
+		if err != nil {
+			// handle error
+		}
+		fmt.Println(string(buf[0:]))
+	}
+
 }
