bfb5d441f49e3a56eabc77983e39378b53a6a86e
Mads Valentin Jensen <mavjdk@gmail.com>
2023-09-23 16:31:34 +0200

    jeg har leget lidt med iden om hvordan vi skal lave 3way handshake når der modtages

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"fmt"
+	"math/rand"
 	"net"
 	"time"
 )
+
+
 
 func main() {
 	go listen(25555)
@@ -28,6 +32,16 @@ func send(port int) {
 	fmt.Print(status)
 }
 
+
+const (
+	SYN  byte = 0b00000001
+	ACK  byte = 0b00000010
+	FIN  byte = 0b00000100
+	DATA byte = 0b00001000
+	SYN_ACK byte = 0b00000011
+)
+
+
 func listen(port int) {
 	udpAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", port))
 	if err != nil {
@@ -37,14 +51,93 @@ func listen(port int) {
 	if err != nil {
 		fmt.Print(err)
 	}
+	ConnectionsState := NewStateMachine()
+	acknowledgementMap := make(map[string]uint32)
+
 	for {
 		var buf [512]byte
-		_, addr, err := ln.ReadFromUDP(buf[0:])
+		n, addr, err := ln.ReadFromUDP(buf[0:])
 		println(addr)
+		println(n)
 		if err != nil {
 			// handle error
 		}
-		fmt.Println(string(buf[0:]))
-	}
+		flags := buf[0] // assuming the first byte contains the flags
+		state := ConnectionsState.GetState(addr)
+		//first check the flag bits
+		switch{
+		//if first syn is recevied return syn + ack
+		case flags&SYN == SYN:
+			if state == ""{
+				ConnectionsState.SetState(addr, StateAwatingACK)
+				acknowledgementINT := rand.Uint32()
+				acknowledgementMap[addr.String()] = acknowledgementINT
 
+				//convert into 4 bytes
+				acknowledgementBytes := make([]byte, 4)
+				binary.LittleEndian.PutUint32(acknowledgementBytes, uint32(acknowledgementINT))
+				header := make([]byte, 5)
+				//add the byte for syn and ack, then add the integer for acknowlegement into a header
+				header[0] = SYN_ACK
+				for i:=1; i<5; i++{
+					header[i] = acknowledgementBytes[i-1]
+				}
+				ln.WriteToUDP(header, addr)
+			}
+		//wait next falg should allways be ACK for the same incoming addr
+		case flags&ACK == ACK:
+			if state == StateAwatingACK{
+				acknowledgementBytes := make([]byte, 4)
+				for i := 1; i < 5 ; i++{
+					acknowledgementBytes[i-1] = buf[i]
+				}					
+				acknowledgementUINT := binary.BigEndian.Uint32(acknowledgementBytes)
+				if acknowledgementMap[addr.String()] == acknowledgementUINT{
+					ConnectionsState.SetState(addr, StateConnectionEstablished)
+				} 
+				//TODO: if the acknowledgement number is not the same handle
+			}
+			if state == StateAwatingFinACK{
+
+			}
+		//now date is ready to be resived after each recived data block return empty responce with flag bit ACK activated
+		
+
+		
+
+		//if fin is recived unless data is missing return ACK
+
+		//then return FIN and wait for ACK
+
+		//now the connection is closed
+		}
+	}
+}
+
+
+//we want to ensure that multible messages can be sent a once 
+type ConnectionState string
+
+const (
+	StateAwatingACK  ConnectionState = "AwaitACK"
+	StateConnectionEstablished ConnectionState = "ConnectionEstablished"
+	StateAwatingFinACK ConnectionState = "AwatingACKForFIN"
+)
+
+type StateMachine struct {
+	ConnectionStates map[string]ConnectionState
+}
+
+func NewStateMachine() *StateMachine {
+	return &StateMachine{
+		ConnectionStates: make(map[string]ConnectionState),
+	}
+}
+
+func (sm *StateMachine) SetState(addr *net.UDPAddr, state ConnectionState) {
+	sm.ConnectionStates[addr.String()] = state
+}
+
+func (sm *StateMachine) GetState(addr *net.UDPAddr) ConnectionState {
+	return sm.ConnectionStates[addr.String()]
 }
