358194f3b2fc1b35be7029d2758f44200e7862c1
Mads Valentin Jensen <mavjdk@gmail.com>
2023-09-24 15:27:14 +0200

    added fixed send function

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"encoding/binary"
 	"fmt"
 	"math/rand"
@@ -9,7 +8,14 @@ import (
 	"time"
 )
 
-
+const (
+	SYN     byte = 0b00000001
+	ACK     byte = 0b00000010
+	FIN     byte = 0b00000100
+	DATA    byte = 0b00001000
+	SYN_ACK byte = 0b00000011
+	//if any bytes are needed it is added here
+)
 
 func main() {
 	go listen(25555)
@@ -22,25 +28,47 @@ func main() {
 }
 
 func send(port int) {
-	conn, err := net.Dial("udp", fmt.Sprintf("localhost:%d", port))
+	// Establish a connection
+	conn, err := net.DialUDP("udp", nil, &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: port})
 	if err != nil {
-		// handle error
+		fmt.Println("Error dialing:", err)
+		return
 	}
-	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-	status, err := bufio.NewReader(conn).ReadString('\n')
 
-	fmt.Print(status)
+	defer conn.Close()
+	// Send SYN packet
+	_, err = conn.Write([]byte{SYN})
+	if err != nil {
+		fmt.Println("Error sending SYN:", err)
+		return
+	}
+	fmt.Println("Sending SYN")
+	// Await SYN-ACK response
+	buf := make([]byte, 512)
+	n, _, err := conn.ReadFromUDP(buf)
+	if err != nil {
+		fmt.Println("Error reading SYN-ACK:", err)
+		return
+	}
+	if n == 0 {
+		return
+	}
+	flags := buf[0]
+	if flags&SYN_ACK == SYN_ACK {
+		header := make([]byte, 5)
+		header[0] = ACK
+		//acknowledgement shoud be in 1-4 in the buffer
+		for i := 1; i < 5; i++ {
+			header[i] = buf[i]
+		}
+		_, err = conn.Write(header)
+		if err != nil {
+			fmt.Println("Error sending ACK:", err)
+			return
+		}
+		fmt.Println("Handshake completed with server.")
+	}
 }
-
-
-const (
-	SYN  byte = 0b00000001
-	ACK  byte = 0b00000010
-	FIN  byte = 0b00000100
-	DATA byte = 0b00001000
-	SYN_ACK byte = 0b00000011
-)
-
 
 func listen(port int) {
 	udpAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", port))
@@ -56,72 +84,67 @@ func listen(port int) {
 
 	for {
 		var buf [512]byte
-		n, addr, err := ln.ReadFromUDP(buf[0:])
-		println(addr)
-		println(n)
+		_, addr, err := ln.ReadFromUDP(buf[0:])
 		if err != nil {
 			// handle error
 		}
 		flags := buf[0] // assuming the first byte contains the flags
 		state := ConnectionsState.GetState(addr)
 		//first check the flag bits
-		switch{
+		switch {
 		//if first syn is recevied return syn + ack
 		case flags&SYN == SYN:
-			if state == ""{
+			if state == "" {
 				ConnectionsState.SetState(addr, StateAwatingACK)
 				acknowledgementINT := rand.Uint32()
 				acknowledgementMap[addr.String()] = acknowledgementINT
 
 				//convert into 4 bytes
 				acknowledgementBytes := make([]byte, 4)
-				binary.LittleEndian.PutUint32(acknowledgementBytes, uint32(acknowledgementINT))
+				binary.BigEndian.PutUint32(acknowledgementBytes, uint32(acknowledgementINT))
 				header := make([]byte, 5)
 				//add the byte for syn and ack, then add the integer for acknowlegement into a header
 				header[0] = SYN_ACK
-				for i:=1; i<5; i++{
+				for i := 1; i < 5; i++ {
 					header[i] = acknowledgementBytes[i-1]
 				}
 				ln.WriteToUDP(header, addr)
 			}
 		//wait next falg should allways be ACK for the same incoming addr
 		case flags&ACK == ACK:
-			if state == StateAwatingACK{
+			if state == StateAwatingACK {
 				acknowledgementBytes := make([]byte, 4)
-				for i := 1; i < 5 ; i++{
+				for i := 1; i < 5; i++ {
 					acknowledgementBytes[i-1] = buf[i]
-				}					
+				}
 				acknowledgementUINT := binary.BigEndian.Uint32(acknowledgementBytes)
-				if acknowledgementMap[addr.String()] == acknowledgementUINT{
+				if acknowledgementMap[addr.String()] == acknowledgementUINT {
 					ConnectionsState.SetState(addr, StateConnectionEstablished)
-				} 
+				}
 				//TODO: if the acknowledgement number is not the same handle
 			}
-			if state == StateAwatingFinACK{
+			if state == StateAwatingFinACK {
 
 			}
-		//now date is ready to be resived after each recived data block return empty responce with flag bit ACK activated
-		
+			//now date is ready to be resived after each recived data block return empty responce with flag bit ACK activated
 
-		
+			//if fin is recived unless data is missing return ACK
 
-		//if fin is recived unless data is missing return ACK
+			//then return FIN and wait for ACK
 
-		//then return FIN and wait for ACK
-
-		//now the connection is closed
+			//now the connection is closed
 		}
 	}
 }
 
-
-//we want to ensure that multible messages can be sent a once 
+// we want to ensure that multible messages can be sent a once
+// only to be used for the server
 type ConnectionState string
 
 const (
-	StateAwatingACK  ConnectionState = "AwaitACK"
+	StateAwatingACK            ConnectionState = "AwaitACK"
 	StateConnectionEstablished ConnectionState = "ConnectionEstablished"
-	StateAwatingFinACK ConnectionState = "AwatingACKForFIN"
+	StateAwatingFinACK         ConnectionState = "AwatingACKForFIN"
 )
 
 type StateMachine struct {
